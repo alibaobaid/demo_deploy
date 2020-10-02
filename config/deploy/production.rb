9fe50095cfcc0ddf3675160bb920eb16757b3935
Ali Amrah <ali.b5556789@gmail.com>
2020-10-02 08:51:13 +0300

    change domain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :branch, 'main'
 set :server_ip, '15.184.49.212'
-set :nginx_server_name, "blog.no-limits.site"
+set :nginx_server_name, "ali.alghreeb.me"
 server fetch(:server_ip), user: 'ubuntu', roles: %w[web app db]
